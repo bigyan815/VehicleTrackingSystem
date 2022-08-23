63a8cb9be6229befa5986d48a09052946f4a3c9d
bigyan815 <bigyantamang.90@gmail.com>
2022-08-23 22:58:46 +0545

    Final code

--- Vehicle_Trakcing_System.ino
+++ Vehicle_Trakcing_System.ino
@@ -1,94 +1,67 @@
+#include <TinyGPS++.h>
 #include <SoftwareSerial.h>
 #define BLYNK_PRINT Serial
-
-//Install the following Libraries
-#include <TinyGPS++.h>
 #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
-
-//GPS RX to D1 & GPS TX to D2 and Serial Connection
-const int RXPin = 4, TXPin = 5;
-const uint32_t GPSBaud = 9600; 
-SoftwareSerial gps_module(RXPin, TXPin);
-
-TinyGPSPlus gps; 
-WidgetMap myMap(V0); //V0 - virtual pin for Map
-
+static const int RXPin = 4, TXPin = 5;  
+static const uint32_t GPSBaud = 9600; 
+TinyGPSPlus gps;
+WidgetMap myMap(V0);  
+SoftwareSerial ss(RXPin, TXPin);  
 BlynkTimer timer;
-
-//Variable  to store the speed, no. of satellites, direction
-float gps_speed;
-float no_of_satellites;
-String satellite_orientation;
-
-char auth[] = "iXIG8PtIKDzeQN0C9emP27tJNU6S4M_a";              
-char ssid[] = "smarthome";
-char pass[] = "Ahmad051";
-
+float spd;       
+float sats;      
+String bearing;  
+char auth[] = "XIIpHMe8qTXOy79ZtDocJD2LcsVx5R1O";
+char ssid[] = "anamolous_2.4";
+char pass[] = "nepal@123";                                         
 //unsigned int move_index;         
-unsigned int move_index = 1;      
-  
-
+unsigned int move_index = 1;       
 void setup()
 {
   Serial.begin(115200);
   Serial.println();
-  gps_module.begin(GPSBaud);
+  ss.begin(GPSBaud);
   Blynk.begin(auth, ssid, pass);
-  timer.setInterval(5000L, checkGPS); 
+  timer.setInterval(5000L, checkGPS); // every 5s check if GPS is connected, only really needs to be done once
 }
-
 void checkGPS(){
   if (gps.charsProcessed() < 10)
   {
     Serial.println(F("No GPS detected: check wiring."));
-      Blynk.virtualWrite(V4, "GPS ERROR");  
+      Blynk.virtualWrite(V4, "GPS ERROR");  // Value Display widget  on V4 if GPS not detected
   }
 }
-
 void loop()
 {
-  while (gps_module.available() > 0) 
-  {
-    //displays information every time a new sentence is correctly encoded.
-    if (gps.encode(gps_module.read()))
-    displayInfo();
+    while (ss.available() > 0) 
+    {
+      // sketch displays information every time a new sentence is correctly encoded.
+      if (gps.encode(ss.read()))
+        displayInfo();
   }
-  
   Blynk.run();
   timer.run();
 }
-
 void displayInfo()
-{
-  if (gps.location.isValid()) 
-  {
-    //Storing the Latitude. and Longitude
-    float latitude = (gps.location.lat());
+{ 
+  if (gps.location.isValid() ) 
+  {    
+    float latitude = (gps.location.lat());     //Storing the Lat. and Lon. 
     float longitude = (gps.location.lng()); 
-    
-    //Send to Serial Monitor for Debugging
     Serial.print("LAT:  ");
     Serial.println(latitude, 6);  // float to x decimal places
     Serial.print("LONG: ");
     Serial.println(longitude, 6);
-    
     Blynk.virtualWrite(V1, String(latitude, 6));   
     Blynk.virtualWrite(V2, String(longitude, 6));  
     myMap.location(move_index, latitude, longitude, "GPS_Location");
-    
-    //get speed
-    gps_speed = gps.speed.kmph();
-    Blynk.virtualWrite(V3, gps_speed);
-       
-    //get number of satellites
-    no_of_satellites = gps.satellites.value();
-    Blynk.virtualWrite(V4, no_of_satellites);
-
-    // get the satellite orientation/direction
-    satellite_orientation = TinyGPSPlus::cardinal(gps.course.value());
-    Blynk.virtualWrite(V5, satellite_orientation);
+    spd = gps.speed.kmph();               //get speed
+       Blynk.virtualWrite(V3, spd);
+       sats = gps.satellites.value();    //get number of satellites
+       Blynk.virtualWrite(V4, sats);
+       bearing = TinyGPSPlus::cardinal(gps.course.value()); // get the direction
+       Blynk.virtualWrite(V5, bearing);                   
   }
-  
-  Serial.println();
+ Serial.println();
 }
